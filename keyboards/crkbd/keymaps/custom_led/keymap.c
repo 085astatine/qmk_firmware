84628fe716830d03787778739d44c3ff87c21c0a
085astatine <astatine085@gmail.com>
2021-07-09 17:15:59 +0900

    Update keymap

--- keyboards/crkbd/keymaps/custom_led/keymap.c
+++ keyboards/crkbd/keymaps/custom_led/keymap.c
@@ -76,9 +76,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_PSCR,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   RESET,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_CAPS,   KC_F5,   KC_F6,   KC_F7,   KC_F8,  KC_INS,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, KC_RALT,
+      XXXXXXX,   KC_F5,   KC_F6,   KC_F7,   KC_F8,  KC_INS,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, KC_RALT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    MO(L_LED),   KC_F9,  KC_F10,  KC_F11,  KC_F12, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LGUI,
+    MO(L_LED),   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_CAPS,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, KC_LGUI,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                            KC_ESC,   LOWER,  KC_SPC,     KC_ENT,   RAISE, KC_RSFT
                                       //`--------------------------'  `--------------------------'
